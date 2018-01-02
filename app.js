caf2f0119a2e3af6d70191beda6bc6e002372e4b
San Guyer <sanguyer@gmail.com>
2018-01-01 20:10:24 -0500

    Setup app.js index.html

---
+++ app.js
@@ -0,0 +1,60 @@
+(function() {
+
+  var app = angular.module('directivesApp', []);
+
+  var injectParams = ['$scope'];
+
+  var customersController = function($scope) {
+    var counter = 0;
+    
+    $scope.tasks = [{ title: 'Task1'}];
+
+    $scope.customer = {
+      name: 'John',
+      street: '123 Main St'
+    };
+
+    $scope.customers = [
+      {
+        name: 'David',
+        street: '111 Lincoln St'
+      },
+      {
+        name: 'Mary',
+        street: '222 Broadway'
+      },
+      {
+        name: 'Jim',
+        street: '333 Cedar Way'
+      },
+      {
+        name: 'Tina',
+        street: '444 5th Ave'
+      },
+      {
+        name: 'Bill',
+        street: '555 Blosom Lane'
+      }
+    ];
+
+    $scope.addCustomer = function(name) {
+      counter++;
+      $scope.customers.push({
+        name: (name) ? name : 'New Cusstomer ' + counter,
+        street: counter + ' Anywhere St'
+      });
+    };
+
+    $scope.changeData = function() {
+      $scope.customer = {
+        name: 'Jimbo',
+        street: '777 Lucky Lane'
+      };
+    };
+
+    customersController.$inject = injectParams;
+
+    app.controller('customersController', customersController);
+
+  }
+}());
